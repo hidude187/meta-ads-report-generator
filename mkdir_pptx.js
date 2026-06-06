b0a84b5ac3eb1acbb07d2313c8fa52137998fd91
BOUREGHDA WAHID <hidude187@hotmail.com>
2026-06-06 17:05:20 +0100

    chore: remove temp scripts

--- mkdir_pptx.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const path = 'C:\\Users\\Wahid\\Desktop\\claude\\projects\\metriquill-free\\lib\\pptx';
-fs.mkdirSync(path, { recursive: true });
-console.log('Created:', path);
